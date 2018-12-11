4e03a72b70614368e248a53cccc2a3cb76a1f670
Josh Glendenning <josh@isobit.io>
2018-12-10 23:14:18 -0600

    Add systemd-boot console mode option for hidpi

--- hidpi.nix
+++ hidpi.nix
@@ -4,8 +4,12 @@
 
   # Larger console font
   i18n.consoleFont = lib.mkDefault "latarcyrheb-sun32";
+
   # Also use large console font in initrd
   boot.earlyVconsoleSetup = true;
+
+  # Try use maximum resolution in systemd-boot
+  boot.loader.systemd-boot.consoleMode = lib.mkDefault "max";
 
   # services.xserver.dpi = 210;
   # fonts.fontconfig.dpi = 210;
