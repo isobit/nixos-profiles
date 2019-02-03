b7da3fd802a9e27ed07c98ce6b50240c9b956dfc
Josh Glendenning <josh@isobit.io>
2019-02-03 13:42:15 -0600

    Rename thinkpad-x1y3 S0i3.nix to S2idle.nix to be more accurate, improve comments

--- thinkpad-x1y3/S2idle.nix
+++ thinkpad-x1y3/S2idle.nix
@@ -1,20 +1,17 @@
 { config, lib, pkgs, ... }:
 
 # S3 sleep is not supported out of the box so a few steps are taken here to
-# mitigate, mainly reducing the power consumption of S0i3 sleep mode and
-# defaulting the lid close action to hibernate.
+# mitigate the downsides of S2idle sleep mode.
 # See the following for more info:
+# https://wiki.archlinux.org/index.php/Lenovo_ThinkPad_X1_Yoga_(Gen_3)#Enabling_S2idle
 # https://wiki.archlinux.org/index.php/Lenovo_ThinkPad_X1_Carbon_(Gen_6)#Suspend_issues
 
 {
-  # Reduces power consumption of S0i3 sleep mode, see the following for
-  # more info and additional power saving tips:
-  # https://wiki.archlinux.org/index.php/Lenovo_ThinkPad_X1_Carbon_(Gen_6)#S0i3_sleep_support
+  # Reduces power consumption of S2idle sleep mode
   boot.kernelParams = [ "acpi.ec_no_wakeup=1" ];
 
   services.logind = {
-    # Mitigate high S0i3 sleep mode power consumption by defaulting the lid
-    # close action to hibernate
+    # Default the lid close action to hibernate
     lidSwitch = lib.mkDefault "hibernate";
 
     # Always hibernate when lid is closed, regardless of sleep state
