45d06b0f18b6bba74e3bf5d2d9a424c99a302c45
Josh Glendenning <git@isobit.io>
2019-10-12 14:26:02 -0500

    Remove thinkpad-x1y3 S3 ACPI fix now that updated BIOS supports S3

--- thinkpad-x1y3/S3.nix
+++ thinkpad-x1y3/S3.nix
@@ -1,13 +1,10 @@
 { config, lib, pkgs, ... }:
 
-# Enables S3 suspend state after patching the ACPI DSDT table using the
-# following instructions: https://delta-xi.net/#056
-# https://wiki.archlinux.org/index.php/Lenovo_ThinkPad_X1_Yoga_(Gen_3)#Suspend_issues
+# Enables S3 suspend state after upgrading the BIOS to at least version 1.33
+# and setting standby to linux mode:
+# https://wiki.archlinux.org/index.php/Lenovo_ThinkPad_X1_Yoga_(Gen_3)#Enabling_S3_(with_BIOS_version_1.33_and_after)
 
 {
-  # TODO: Build as part of package or something
-  boot.initrd.prepend = [ "${/etc/nixos/acpi_s3_fix}" ];
-
   boot.kernelParams = [ "mem_sleep_default=deep" ];
 
   services.logind.lidSwitch = lib.mkDefault "suspend";
