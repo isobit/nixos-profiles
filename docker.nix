dadc8e80b3f359a3399fd659f9e8aecc66c7b13c
Josh Glendenning <git@isobit.io>
2020-06-05 18:35:43 -0500

    Add Docker deamon.json config with non-VPN-conflicting network config

--- docker.nix
+++ docker.nix
@@ -4,6 +4,16 @@
   virtualisation.docker = {
     enable = true;
     enableOnBoot = false;
+    # Equivalent of /etc/docker/daemon.json
+    extraOptions = "--config-file=${pkgs.writeText "daemon.json" (builtins.toJSON {
+      # Fix for route conflicts with VPNs, which typically operate in the
+      # 172.16.0.0/12 space
+      bip = "192.168.253.0/23";
+      default-address-pools = [{
+        base = "192.168.254.0/23";
+        size = 27;
+      }];
+    })}";
   };
 
   users.users = {
