bd0925f351b704cd74a5602cbf87f67335b02641
Josh Glendenning <git@isobit.io>
2020-07-02 10:30:49 -0500

    Minor comment improvements

--- docker.nix
+++ docker.nix
@@ -3,11 +3,13 @@
 {
   virtualisation.docker = {
     enable = true;
+    # Use socket activation by default, override in configuration.nix if
+    # desired.
     enableOnBoot = false;
     # Equivalent of /etc/docker/daemon.json
     extraOptions = "--config-file=${pkgs.writeText "daemon.json" (builtins.toJSON {
       # Fix for route conflicts with VPNs, which typically operate in the
-      # 172.16.0.0/12 space
+      # 172.16.0.0/12 space.
       bip = "192.168.253.0/23";
       default-address-pools = [{
         base = "192.168.254.0/23";
@@ -17,6 +19,7 @@
   };
 
   users.users = {
+    # Add users to docker group to use docker without sudo
     josh = {
       extraGroups = [ "docker" ];
     };
