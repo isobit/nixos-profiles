19d15f1d361d76d31c006c3368f50c78523f72cc
Josh Glendenning <git@isobit.io>
2020-07-02 10:30:32 -0500

    Add sound-output-device-chooser gnome extension

--- desktop.nix
+++ desktop.nix
@@ -3,6 +3,7 @@
 {
   environment.systemPackages = with pkgs; [
     gnomeExtensions.dash-to-panel
+    gnomeExtensions.sound-output-device-chooser
     gnomeExtensions.system-monitor
     numix-cursor-theme
     numix-icon-theme-square
