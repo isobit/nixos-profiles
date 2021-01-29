b1f0421f1c9c2fbfd810385d3149f8a42199b878
Josh Glendenning <git@isobit.io>
2021-01-29 16:55:36 -0600

    Add flameshot keybindings to GNOME by default

--- flameshot.nix
+++ flameshot.nix
@@ -6,4 +6,16 @@
     gnomeExtensions.appindicator
   ];
   services.dbus.packages = with pkgs; [ flameshot ];
+  services.xserver.desktopManager.gnome3.extraGSettingsOverrides = ''
+    [org.gnome.settings-daemon.plugins.media-keys]
+    screenshot=[]
+
+    [org.gnome.settings-daemon.plugins.media-keys]
+    custom-keybindings=['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom-flameshot/']
+
+    [org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom-flameshot/]
+    name='flameshot'
+    command='flameshot gui'
+    binding='Print'
+  '';
 }
