845a740042dadc6e304966f5c95dee09e7eadac9
Josh Glendenning <git@isobit.io>
2022-03-28 13:37:15 -0500

    Add neovim profile

---
+++ neovim.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, ... }:
+
+let
+  unstable = import
+    (builtins.fetchTarball https://github.com/nixos/nixpkgs/tarball/nixos-unstable)
+    { config = config.nixpkgs.config; };
+
+  neovimCustom = unstable.neovim;
+  # neovimCustom = let pkgs = unstable; in (pkgs.neovim.override {
+  #     configure = {
+  #       customRC = ''
+  #         source ~/.vimrc
+  #       '';
+  #       packages.myPlugins = with pkgs.vimPlugins; {
+  #         start = [
+  #           (nvim-treesitter.withPlugins ( plugins: pkgs.tree-sitter.allGrammars))
+  #         ];
+  #       };
+  #     };
+  #   });
+in
+{
+  environment.systemPackages = with pkgs; [
+    neovimCustom
+
+    neovide # nice client with fancy animations
+
+    # language servers
+    efm-langserver       # wraps other linting tools
+    gopls                # golang
+    sqls                 # SQL
+    terraform-ls         # terraform
+    yaml-language-server # YAML
+
+    # general purpose linting tools (used by efm-langserver)
+    checkmake               # Makefile
+    hadolint                # Dockerfile
+    jq                      # JSON
+    rubyPackages.solargraph # ruby
+    yamllint                # YAML
+  ];
+}
