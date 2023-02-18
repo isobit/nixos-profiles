1abe5d6cd3f806c369c44021b4a1c1f52374f8c3
Josh Glendenning <git@isobit.io>
2023-02-18 11:07:19 -0600

    Comment out x1y3 s3 resume fixed; might no longer be needed

--- hardware/thinkpad-x1y3/S3.nix
+++ hardware/thinkpad-x1y3/S3.nix
@@ -9,26 +9,26 @@
 
   services.logind.lidSwitch = lib.mkDefault "suspend";
 
-  # Automatically fixes some issue that arise when resuming from sleep
-  systemd.services.thinkpad-x1y3-s3-resume-fix = {
-    enable = true;
-    description = "ThinkPad X1Y3 S3 Resume Fix";
-    documentation = [
-      "https://forums.lenovo.com/t5/Linux-Discussion/X1Y3-Touchscreen-not-working-after-resume-on-Linux/m-p/4149363/highlight/true#M11427"
-    ];
-    wantedBy = [ "suspend.target" ];
-    after = [ "suspend.target" ];
-    serviceConfig = { Type = "oneshot"; };
-    path = [ pkgs.utillinux pkgs.kmod ];
-    script = ''
-      sleep 1
+  # # Automatically fixes some issue that arise when resuming from sleep
+  # systemd.services.thinkpad-x1y3-s3-resume-fix = {
+  #   enable = true;
+  #   description = "ThinkPad X1Y3 S3 Resume Fix";
+  #   documentation = [
+  #     "https://forums.lenovo.com/t5/Linux-Discussion/X1Y3-Touchscreen-not-working-after-resume-on-Linux/m-p/4149363/highlight/true#M11427"
+  #   ];
+  #   wantedBy = [ "suspend.target" ];
+  #   after = [ "suspend.target" ];
+  #   serviceConfig = { Type = "oneshot"; };
+  #   path = [ pkgs.utillinux pkgs.kmod ];
+  #   script = ''
+  #     sleep 1
 
-      # Fix touchscreen
-      rtcwake -m freeze -s 1
+  #     # Fix touchscreen
+  #     rtcwake -m freeze -s 1
 
-      # Fix touchpad
-      modprobe -r psmouse
-      modprobe psmouse
-    '';
-  };
+  #     # Fix touchpad
+  #     modprobe -r psmouse
+  #     modprobe psmouse
+  #   '';
+  # };
 }
