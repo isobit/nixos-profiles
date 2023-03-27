645cf8fe6ce8930dcf42c97d22f482cd6fdf4d79
Josh Glendenning <git@isobit.io>
2023-03-26 23:38:54 -0500

    Update neovim config

--- neovim.nix
+++ neovim.nix
@@ -4,8 +4,6 @@
   environment.systemPackages = with pkgs; [
     neovim
 
-    neovide # nice client with fancy animations
-
     # language servers
     efm-langserver       # wraps other linting tools
     gopls                # golang
@@ -13,15 +11,14 @@
     sqls                 # SQL
     terraform-ls         # terraform
     yaml-language-server # YAML
-    # python-language-server # Python
     python3Packages.python-lsp-server # Python
 
     # general purpose linting tools (used by efm-langserver)
     checkmake               # Makefile
     hadolint                # Dockerfile
     jq                      # JSON
-    rubyPackages.solargraph # ruby
-    yamllint                # YAML
-    deno                    # Javascript/TypeScript
+    # rubyPackages.solargraph # ruby
+    # yamllint                # YAML
+    # deno                    # Javascript/TypeScript
   ];
 }
