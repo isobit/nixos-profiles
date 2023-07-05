e2fc17e6f62f9a7127795740ef86a826320c58ff
Josh Glendenning <git@isobit.io>
2023-07-05 16:44:49 -0500

    Add nix-direnv profile

---
+++ nix-direnv.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [ direnv nix-direnv ];
+  # nix options for derivations to persist garbage collection
+  nix.settings = {
+    keep-outputs = true;
+    keep-derivations = true;
+  };
+  environment.pathsToLink = [
+    "/share/nix-direnv"
+  ];
+  # if you also want support for flakes
+  nixpkgs.overlays = [
+    (self: super: { nix-direnv = super.nix-direnv.override { enableFlakes = true; }; } )
+  ];
+}
