dc29ea6ae6574b3558fd230eb896040ec9992f5f
Josh Glendenning <git@isobit.io>
2025-06-28 19:30:16 -0500

    Fix platformio

--- platformio.nix
+++ platformio.nix
@@ -2,10 +2,10 @@
 
 {
   environment.systemPackages = with pkgs; [ platformio ];
-  services.udev.packages = with pkgs; [ platformio ];
+  services.udev.packages = with pkgs; [ platformio-core.udev openocd ];
   users.users.josh.extraGroups = [ "dialout" ];
-  networking.firewall = {
-    allowedTCPPorts = [ 8266 ];
-    allowedUDPPorts = [ 8266 ];
-  };
+  # networking.firewall = {
+  #   allowedTCPPorts = [ 8266 ];
+  #   allowedUDPPorts = [ 8266 ];
+  # };
 }
