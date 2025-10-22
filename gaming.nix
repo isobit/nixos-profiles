8feb90f85a4ec243a1b34dc84399143e34209f08
Josh Glendenning <git@isobit.io>
2025-10-22 00:19:45 -0500

    Gamemode and pipewire fixes in gaming profile

--- gaming.nix
+++ gaming.nix
@@ -2,4 +2,15 @@
 {
   programs.steam.enable = true;
   hardware.xone.enable = true;
+  programs.gamemode.enable = true;
+
+  # Attempt to fix audio issues
+  services.pipewire.extraConfig.pipewire."92-low-latency" = {
+    "context.properties" = {
+      "default.clock.rate" = 48000;
+      "default.clock.quantum" = 512;  # Start here and increase if needed
+      "default.clock.min-quantum" = 512;
+      "default.clock.max-quantum" = 2048;
+    };
+  };
 }
