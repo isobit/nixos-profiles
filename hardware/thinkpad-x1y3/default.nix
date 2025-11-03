80008c872660e9ca7f6ae133ac82dde5b72e6191
Josh Glendenning <git@isobit.io>
2025-11-03 09:43:29 -0600

    Update thinkpad-x1y3 config

--- hardware/thinkpad-x1y3/default.nix
+++ hardware/thinkpad-x1y3/default.nix
@@ -1,8 +1,29 @@
 { config, lib, pkgs, ... }:
 
 {
-  # Requires github.com/NixOS/nixos-hardware channel
-  imports = [
-    <nixos-hardware/lenovo/thinkpad/x1/yoga>
-  ];
+  # Improved SSD performance
+  services.fstrim.enable = true;
+
+  # automatic screen orientation
+  hardware.sensor.iio.enable = true;
+
+  # Fingerprint reader support
+  services.fprintd.enable = true;
+
+  # Firmware updates
+  services.fwupd.enable = true;
+
+  # Touchscreen support
+  services.xserver.wacom.enable = true;
+
+  # Enables S3 suspend state after upgrading the BIOS to at least version 1.33
+  # and setting standby to linux mode:
+  # https://wiki.archlinux.org/index.php/Lenovo_ThinkPad_X1_Yoga_(Gen_3)#Enabling_S3_(with_BIOS_version_1.33_and_after)
+  boot.kernelParams = [ "mem_sleep_default=deep" ];
+
+  services.logind.lidSwitch = "suspend-then-hibernate";
+
+  systemd.sleep.extraConfig = ''
+    HibernateDelaySec=1h
+  '';
 }
