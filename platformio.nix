db15ab8aceb17cda749e4c4a6917bd8d5725fcd6
Josh Glendenning <git@isobit.io>
2026-07-04 01:40:27 -0500

    platformio: squash error logs about plugdev

--- platformio.nix
+++ platformio.nix
@@ -3,7 +3,10 @@
 {
   environment.systemPackages = with pkgs; [ platformio ];
   services.udev.packages = with pkgs; [ platformio-core.udev openocd ];
-  users.users.josh.extraGroups = [ "dialout" ];
+  users.users.josh.extraGroups = [
+    "dialout"
+    "plugdev" # not strictly necessary but avoids error logs in journal
+  ];
   # networking.firewall = {
   #   allowedTCPPorts = [ 8266 ];
   #   allowedUDPPorts = [ 8266 ];
